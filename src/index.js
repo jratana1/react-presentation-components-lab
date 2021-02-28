bdea016dbc4f48221b2b39ba1d3c0720087b5cc3
John Ratana <jratana1@gmail.com>
2021-02-28 13:13:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=> alert("say hi to your mom")} />
   </div>,
   document.getElementById('root')
 );
