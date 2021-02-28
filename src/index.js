60b0d4f6952e1dfffa29ac509cd4e359acb39474
John Ratana <jratana1@gmail.com>
2021-02-28 13:10:28 -0500

    commit tree e37150f7349fe8ce0bdae4020614ef2c9063d224

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=> "say hi to your mom"} />
   </div>,
   document.getElementById('root')
 );
